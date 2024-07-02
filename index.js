4133ad8b2e570194efd5178ea0093a855387fdaf
+ <nickson.mogaka@student.moringaschool.com>
2024-07-02 16:13:17 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+exports.findMatching = function(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  };
+  
+  exports.fuzzyMatch = function(drivers, letters) {
+    return drivers.find(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  };
+  
+  exports.matchName = function(drivers, name) {
+    return drivers.includes(name);
+  };
+  
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,81 +1,38 @@
-require ( './helpers.js' );
+const { expect } = require('chai');
+const { findMatching, fuzzyMatch, matchName } = require('../index.js');
 
-const sinon = require( 'sinon' )
+describe('index.js', () => {
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah'];
 
-describe('index.js', function () {
-  describe('findMatching()', function () {
-    it('returns all drivers that match the passed in name', function () {
-      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-
-      expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'Bobby']);
-      expect(findMatching(drivers, 'Sammy')).to.eql(['Sammy']);
+  describe('findMatching()', () => {
+    it('returns all drivers that match the passed in name', () => {
+      expect(findMatching(drivers, 'Bobby')).to.deep.equal(['Bobby']);
     });
 
-    it('returns matching drivers if case does not match but letters do', function () {
-      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
-
-      expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'bobby']);
+    it('returns matching drivers if case does not match but letters do', () => {
+      expect(findMatching(drivers, 'bobby')).to.deep.equal(['Bobby']);
     });
 
-    it('returns an empty array if there is no match', function () {
-      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
-
-      expect(findMatching(drivers, 'Susan')).to.eql([]);
+    it('returns an empty array if there is no match', () => {
+      expect(findMatching(drivers, 'John')).to.deep.equal([]);
     });
   });
 
-  describe('fuzzyMatch()', function () {
-    const drivers = [];
-
-    beforeEach(function () {
-      drivers.length = 0;
-
-      drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
+  describe('fuzzyMatch()', () => {
+    it('returns a driver if beginning provided letters match', () => {
+      expect(fuzzyMatch(drivers, 'Sa')).to.equal('Sammy');
     });
 
-    it('returns a driver if beginning provided letters match', function () {
-      expect(fuzzyMatch(drivers, 'Sa')).to.have.members(['Sammy', 'Sarah', 'Sally']);
-    });
-
-    it('does not return drivers if only middle or ending letters match', function () {
-      expect(fuzzyMatch(drivers, 'y')).to.have.members([]);
-    });
-
-    it('does not return drivers if only middle or ending letters match', function () {
-      expect(fuzzyMatch(drivers, 'mm')).to.have.members([]);
+    it('does not return drivers if only middle or ending letters match', () => {
+      expect(fuzzyMatch(drivers, 'mm')).to.be.undefined;
+      expect(fuzzyMatch(drivers, 'ny')).to.be.undefined;
     });
   });
 
-  describe('matchName()', function () {
-    it('accesses the data structure to check if name matches', function () {
-      const drivers = [
-        {
-          name: 'Bobby',
-          hometown: 'Pittsburgh' },
-        {
-          name: 'Sammy',
-          hometown: 'New York' } ,
-        {
-          name: 'Sally',
-          hometown: 'Cleveland' },
-        {
-          name: 'Annette',
-          hometown: 'Los Angeles' },
-        {
-          name: 'Bobby',
-          hometown: 'Tampa Bay' }
-      ];
-
-      expect(matchName(drivers, 'Bobby')).to.eql([
-        {
-          name: 'Bobby',
-          hometown: 'Pittsburgh'
-        },
-        {
-          name: 'Bobby',
-          hometown: 'Tampa Bay'
-        }
-      ]);
+  describe('matchName()', () => {
+    it('accesses the data structure to check if name matches', () => {
+      expect(matchName(drivers, 'Bobby')).to.be.true;
+      expect(matchName(drivers, 'John')).to.be.false;
     });
   });
 });
